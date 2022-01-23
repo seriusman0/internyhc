9d4a8a1a7f9401c8d1a3c81d41da54a162edaf79
serius <seriusman0@gmail.com>
2022-01-24 00:30:37 +0700

    fist time to push

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+	$server = 'localhost';
+	$username = 'serius';
+	$password = 'tmDevFlats0987^(';
+	$database = 'internyhc';
+
+	$conn = mysqli_connect($server, $username, $password, $database) or die("Failed to connect to database");
+
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	include "config.php";
+?>
+
+<html>
+	<head>
+		<title>Intern YHC|Serius</title>
+	</head>
+	<body>
+		<h1>thi is for my testing</h1>
+	</body>
+</html>
